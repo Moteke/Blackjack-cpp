3916b71aba883c1e21625afb2bd62d6c8bdf37e7
Piotr Politowicz <moteke06@gmail.com>
2021-04-17 15:13:27 +0200

    Implemented outputCards()

--- BlackjackIO.cpp
+++ BlackjackIO.cpp
@@ -1,5 +1,12 @@
 #include "BlackjackIO.h"
 
 namespace blackjackIO {
-
+	void outputCards(Player& p)
+	{
+		std::cout << p.name << "'s cards (sum: " << p.sum <<"): ";
+		for (auto card : p.cards) {
+			std::cout << card << " ";
+		}
+		std::cout << '\n';
+	}
 }
--- BlackjackIO.h
+++ BlackjackIO.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "Player.h"
+
+#include <iostream>
 
 namespace blackjackIO {
+
+	void outputCards(Player& p);
 
 }
